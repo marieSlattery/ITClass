a9b3b02361eb6ccce3f8bcfa83af0bd1905c956f
Marie Slattery O Carroll <t00189554@365s.ittralee.ie>
2016-10-20 09:34:33 +0100

    testfile

---
+++ rainfallarray.java
@@ -0,0 +1,26 @@
+//rainfallarray.java
+/*This program creates an array of rainfall values and determines some statistaical
+ *information based on it*/
+ 
+ import javax.swing.JOptionPane;
+ import java.awt.Font;
+ import javax.swing.JTextArea
+ 	
+ public class{
+ 
+ 	public static void main (String args[])
+ 	{
+ 	
+ 		int rainfall[] = (7, 12, 10, 4, 5, 3, 1, 4, 3, 7, 8, 10);
+ 		int month, i, janToJuneRainfall=0, winterRainfall;
+ 		String text ="", monthAsString;
+ 		
+ 		Font textAreaFont = new Font ("monospaced", Font.PLAIN, 12);
+ 		JTextArea textArea = "", monthAsString;
+ 		
+ 		textArea.setFont (textAreaFont);
+ 		
+ 	
+ 		
+ 	}
+ 	}
